9550bfb12b207f553930b25d4187cad1c3dffc2a
Celia <celiinavarroperez@gmail.com>
2020-05-11 19:29:13 +0100

    Style: first commit

--- resources/views/ticket.blade.php
+++ resources/views/ticket.blade.php
@@ -8,17 +8,10 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <link href ="{{ asset('css/style.css') }}" rel="stylesheet">
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+            
 
             .full-height {
                 height: 100vh;
