b103d6b60ab421e9299f5e8f2dec6796b47dbd56
berlen15 <belenpulidorivero13@gmail.com>
2020-05-11 11:59:50 +0100

    Correción de rutas para la conexión entre las páginas principales (/home y /login) con el usuario empresa. RAMA ADD ACTIVITY

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 
 /*rutas para actividades (listar, añadir, modificar y eliminar)*/
-Route::get('/activities/{companyid}/list', 'ActivityController@listActivitiesCompany')->name('listActivities'); //listado actividades propias empresa
-Route::get('/activities/{companyid}/create', 'ActivityController@create')->name('create'); //Vista añadir actividades
-Route::post('/activities/{companyid}/list', 'ActivityController@store')->name('store'); //Método para guardar actividad en BD
+Route::get('/home/activities/{companyid}/list', 'ActivityController@listActivitiesCompany')->name('listActivities'); //listado actividades propias empresa
+Route::get('/home/activities/{companyid}/create', 'ActivityController@create')->name('create'); //Vista añadir actividades
+Route::post('/home/activities/{companyid}/list', 'ActivityController@store')->name('store'); //Método para guardar actividad en BD
 
-Route::get('/activities/{companyid}/edit', 'ActivityController@edit')->name('edit'); //Vista para modificar actividades
-Route::put('/activities/{companyid}', 'ActivityController@update')->name('update'); //Método para modificar actividad en BD
+Route::get('/home/activities/{companyid}/edit', 'ActivityController@edit')->name('edit'); //Vista para modificar actividades
+Route::put('/home/activities/{companyid}', 'ActivityController@update')->name('update'); //Método para modificar actividad en BD
 Route::delete('/activities/{companyid}', 'ActivityController@delete')->name('delete'); //Método para eliminar
