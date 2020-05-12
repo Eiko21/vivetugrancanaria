ce84dc6620d210c97bc0ee53a5af8a8cc0caeb5a
berlen15 <belenpulidorivero13@gmail.com>
2020-05-12 15:52:05 +0100

    Estilo finalizado.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,27 +1,13 @@
-<footer class="footer mt-auto py-3">
-  <div class="container">
-  <center>
-  <div class="mini-footer">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="copyright-text">
-            <p>© 2020
+<center>
+    <footer class="footer">
+      <div class="container">
+        <p>© 2020
               <a href="#">ViveTuGranCanaria</a>. Todos los derechos reservados. Creado por
               <a href="#">MdaCompany | Grupo02</a>
             </p>
-          </div>
-
-          <div class="go_top">
-            <span class="icon-arrow-up"></span>
-          </div>
-        </div>
       </div>
-    </div>
-  </div>
-  </center>
-  </div>
-</footer>
+    </footer>
+</center>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script>window.jQuery || document.write('<script src="/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/docs/4.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
