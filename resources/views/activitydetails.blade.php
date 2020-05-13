4bcf652fa1c523349eef3b7bb977540bd17d1d28
Zeida <zeidaromen@gmail.com>
2020-05-13 13:52:29 +0100

    vistas preparadas para añadir header footer

--- resources/views/activitydetails.blade.php
+++ resources/views/activitydetails.blade.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.8.1/css/all.css' integrity='sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf' crossorigin='anonymous'>
-        
-         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-         <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-
-        </head>
-    <body>
+@extends('layouts.app')
+@section('content')
     <div class="container">
                 
         <div>
@@ -57,5 +42,4 @@
                 </div>
         </div> 
         
-  </body>
-</html>  
+    @endsection
