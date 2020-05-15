fe08d8a379217d7e0095a4c05b3bd8e62a988d78
berlen15 <belenpulidorivero13@gmail.com>
2020-05-15 20:28:01 +0100

    no message

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,69 @@
+ <!-- Fixed navbar -->
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-info">
+    <a class="navbar-brand" href="#">Vive tu Gran Canaria</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav mr-auto">
+        @guest
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Actividades</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            @if (Route::has('register'))
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </li>
+            @endif
+        @else
+            @if(Auth::user()->role === ('empresa'))
+                            
+            <li class="nav-item active">                                
+                <a class="nav-link" href="#">Listado Actividades</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Listado Usuarios</a>
+            </li>
+            @endif
+            @if(Auth::user()->role === ('admin'))
+
+            <li class="nav-item active">                                
+                <a class="nav-link" href="#">Listado Usuarios</a>
+            </li>
+            @endif
+            @if(Auth::user()->role === ('cliente'))
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Actividades</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Perfil</a>
+                </li>
+            @endif
+                <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                         </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                             onclick="event.preventDefault();
+                                      document.getElementById('logout-form').submit();">
+                                      {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+    </ul>
+      <form class="form-inline mt-2 mt-md-0">
+        <input class="form-control mr-sm-2" type="text" placeholder="Busca alguna actividad" aria-label="Search">
+        <button type="button" class="btn bg-info"><i class="fas fa-search"></i></button>
+      </form>
+    </div>
+  </nav>
