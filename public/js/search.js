d4e6fede138c9396a4153a232cfda3f525b6f940
Cris <barcelona1252@gmail.com>
2020-05-21 18:25:10 +0100

    Perfeccionamiento de la funcionalidad del buscador

--- public/js/search.js
+++ public/js/search.js
@@ -1,8 +1,7 @@
 function activityFilter(){
     let input_value = $('#search').val();
     $('.card').each(function(){
-        let activity_name = $(this).find('.card-header h4 .activity-name').html();                     
-        if(activity_name.indexOf(input_value) == -1) $(this).hide();
-        else $(this).show();
+        let activity_name = $(this).find('.card-header h4 .activity-name').html();
+        activity_name.indexOf(input_value) == -1 ? $(this).hide() : $(this).show();
     });
 }
